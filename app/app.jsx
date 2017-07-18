487969896b6a831b65264292a060cdc3000622b3
Cody Condon <codycondon50@gmail.com>
2017-07-18 19:33:44 -0400

    init commit

---
+++ app/app.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+//app css
+require('style!css!sass!applicationStyles')
+
+
+ReactDOM.render(
+  <p>Boilerplate 3 project</p>,
+  document.getElementById('app')
+);
